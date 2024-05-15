0aad2f0dc0cd2b6ec73fa0883f652cefa096ff4f
sreenathprem <sreenath.premnadh@hashicorp.com>
2024-05-15 16:16:12 +0200

    Converting to data

--- main.tf
+++ main.tf
@@ -22,36 +22,35 @@ resource "vault_generic_endpoint" "vault_signed_ssh_certs" {
   disable_delete = true
 }
 
-resource "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "example" {
   name     = "vault-education"
-  location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = data.data.azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "example" {
   name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
@@ -63,8 +62,8 @@ resource "azurerm_network_interface" "example" {
 
 resource "azurerm_linux_virtual_machine" "example" {
   name                  = "example-vm"
-  resource_group_name   = azurerm_resource_group.example.name
-  location              = azurerm_resource_group.example.location
+  resource_group_name   = data.azurerm_resource_group.example.name
+  location              = data.azurerm_resource_group.example.location
   size                  = "Standard_B2s"
   admin_username        = "azureuser"
   network_interface_ids = [azurerm_network_interface.example.id]
